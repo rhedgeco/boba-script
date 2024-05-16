ca9b2453dd2c3f59d84a5008c19b984968ca4304
Ryan Hedgecock <rhedgeco@gmail.com>
2024-05-16 13:24:34 -0700

    simplify variable mutation api

--- src/engine/scope.rs
+++ src/engine/scope.rs
@@ -18,21 +18,15 @@ impl Scope {
         self.vars.contains_key(ident)
     }
 
-    pub fn get_var(&self, ident: &Ident) -> Option<&Value> {
-        self.vars.get(ident)
-    }
-
     pub fn init_var(&mut self, ident: Ident, value: Value) {
         self.vars.insert(ident, value);
     }
 
-    pub fn set_var(&mut self, ident: &Ident, new_value: Value) -> Option<Value> {
-        match self.vars.get_mut(ident) {
-            None => Some(new_value),
-            Some(value) => {
-                *value = new_value;
-                None
-            }
-        }
+    pub fn get_var(&self, ident: &Ident) -> Option<&Value> {
+        self.vars.get(ident)
+    }
+
+    pub fn get_var_mut(&mut self, ident: &Ident) -> Option<&mut Value> {
+        self.vars.get_mut(ident)
     }
 }
