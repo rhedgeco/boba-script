8601119de5c5bf8c628bbeaa1490795df940cedd
Ryan Hedgecock <rhedgeco@gmail.com>
2024-09-26 18:16:15 -0700

    add while loop test

--- interpreter/src/run.rs
+++ interpreter/src/run.rs
@@ -1,8 +1,13 @@
 use std::{fs, path::PathBuf};
 
-use boba_script::lexer::LexerState;
+use boba_script::{
+    engine::{scope::LocalScope, Eval},
+    lexer::LexerState,
+    parser::{grammar, spanned::SpannedLexer},
+};
 
 pub fn file(path: PathBuf) {
+    // load file
     let name = path.to_string_lossy();
     let text = match fs::read_to_string(&path) {
         Ok(text) => text,
@@ -12,10 +17,32 @@ pub fn file(path: PathBuf) {
         }
     };
 
-    for result in LexerState::new().lex(&text).filtered() {
-        match result {
-            Ok(token) => println!("{token}"),
-            Err(error) => eprintln!("{error}"),
+    // skip empty input
+    if text.trim().len() == 0 {
+        println!("file is empty");
+        return;
+    }
+
+    // parse the text
+    let mut state = LexerState::new();
+    let lexer = SpannedLexer::new(state.lex(&text));
+    let statements = match grammar::FileParser::new().parse(&text, lexer) {
+        Ok(statements) => statements,
+        Err(error) => {
+            eprintln!("{error}");
+            return;
+        }
+    };
+
+    // execute the file
+    let mut scope = LocalScope::new();
+    for statement in statements {
+        match statement.eval(&mut scope) {
+            Ok(value) => println!("{value}"),
+            Err(error) => {
+                eprintln!("{error}");
+                return;
+            }
         }
     }
 }
