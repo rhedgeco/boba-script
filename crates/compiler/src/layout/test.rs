7c1cd437d96d40e5c6ebf9d849d9c61fd14a03f4
Ryan Hedgecock <rhedgeco@gmail.com>
2024-11-26 12:37:07 -0800

    improve conflict test

--- crates/compiler/src/layout/test.rs
+++ crates/compiler/src/layout/test.rs
@@ -5,26 +5,24 @@ use crate::{layout::LayoutError, ProgramLayout};
 #[test]
 fn insert_conflict() {
     let vis = Node::build(Visibility::Private);
-    let name = Node::build("class0".to_string());
+    let name1 = Node::build("class0".to_string());
+    let name2 = Node::build("class0".to_string());
     let ast = Node::build(Class {
         fields: vec![],
         defs: vec![],
     });
 
     let mut layout = ProgramLayout::new();
-    if let Err(errors) = layout.insert_root_class(&vis, &name, &ast) {
-        panic!("failed to insert class: {errors:?}");
-    }
-
-    let Err(errors) = layout.insert_root_class(&vis, &name, &ast) else {
-        panic!("duplicate class names should conflict");
+    layout.insert_root_class(&vis, &name1, &ast).unwrap();
+    let Err(errors) = layout.insert_root_class(&vis, &name2, &ast) else {
+        panic!("duplicate class names should create a conflict");
     };
 
     assert_eq!(
         errors[0],
         LayoutError::ClassAlreadyExists {
-            insert: name.id(),
-            found: name.id()
+            insert: name2.id(),
+            found: name1.id()
         }
     )
 }
