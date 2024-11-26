f0f10891c907d97363ecc1e7ae78b4af9bb73fd6
Ryan Hedgecock <rhedgeco@gmail.com>
2024-11-26 10:31:32 -0800

    search all parents of a scope when searching for an identifier

--- crates/compiler/src/utils.rs
+++ crates/compiler/src/utils.rs
@@ -3,6 +3,84 @@ use crate::{
     CompileError, ProgramLayout,
 };
 
+pub fn find_module(
+    layout: &ProgramLayout,
+    from_scope: ScopeIndex,
+    name: impl AsRef<str>,
+) -> Option<ScopeIndex> {
+    // ensure from_scope is valid for layout
+    assert!(
+        layout.get_scope(from_scope).is_some(),
+        "from_scope is invalid for this layout"
+    );
+
+    // search up the chain of parents to find the scope
+    let name = name.as_ref();
+    let mut resolve_scope = from_scope;
+    loop {
+        let scope = &layout[resolve_scope];
+        match scope.modules.get(name) {
+            Some(found_scope) => return Some(*found_scope),
+            None => match scope.parent_scope {
+                Some(parent) => resolve_scope = parent,
+                None => return None,
+            },
+        }
+    }
+}
+
+pub fn find_class(
+    layout: &ProgramLayout,
+    from_scope: ScopeIndex,
+    name: impl AsRef<str>,
+) -> Option<ClassIndex> {
+    // ensure from_scope is valid for layout
+    assert!(
+        layout.get_scope(from_scope).is_some(),
+        "from_scope is invalid for this layout"
+    );
+
+    // search up the chain of parents to find the scope
+    let name = name.as_ref();
+    let mut resolve_scope = from_scope;
+    loop {
+        let scope = &layout[resolve_scope];
+        match scope.classes.get(name) {
+            Some(found_class) => return Some(*found_class),
+            None => match scope.parent_scope {
+                Some(parent) => resolve_scope = parent,
+                None => return None,
+            },
+        }
+    }
+}
+
+pub fn find_func(
+    layout: &ProgramLayout,
+    from_scope: ScopeIndex,
+    name: impl AsRef<str>,
+) -> Option<FuncIndex> {
+    // ensure from_scope is valid for layout
+    assert!(
+        layout.get_scope(from_scope).is_some(),
+        "from_scope is invalid for this layout"
+    );
+
+    // search up the chain of parents to find the scope
+    let name = name.as_ref();
+    let mut resolve_scope = from_scope;
+    loop {
+        let scope = &layout[resolve_scope];
+        match scope.funcs.get(name) {
+            Some(found_class) => return Some(*found_class),
+            None => match scope.parent_scope {
+                Some(parent) => resolve_scope = parent,
+                None => return None,
+            },
+        }
+    }
+}
+
 pub fn resolve_module<S: AsRef<str>>(
     layout: &ProgramLayout,
     from_scope: ScopeIndex,
@@ -15,22 +93,22 @@ pub fn resolve_module<S: AsRef<str>>(
     );
 
     // resolve the module path to a scope
-    let mut resolved_scope = from_scope;
+    let mut resolve_scope = from_scope;
     for part in module_path {
         match part.as_ref() {
-            "super" => match layout[resolved_scope].super_scope {
+            "super" => match layout[resolve_scope].super_scope {
                 None => return Err(CompileError::SuperFromRootScope),
-                Some(super_scope) => resolved_scope = super_scope,
+                Some(super_scope) => resolve_scope = super_scope,
             },
-            part => match layout[resolved_scope].modules.get(part) {
+            part => match find_module(layout, resolve_scope, part) {
                 None => return Err(CompileError::ModuleDoesNotExist),
-                Some(module_scope) => resolved_scope = *module_scope,
+                Some(module_scope) => resolve_scope = module_scope,
             },
         }
     }
 
     // return the resolved scope
-    Ok(resolved_scope)
+    Ok(resolve_scope)
 }
 
 pub fn resolve_class<S: AsRef<str>>(
@@ -52,9 +130,9 @@ pub fn resolve_class<S: AsRef<str>>(
 
     // resolve the module and class path
     let module_scope = resolve_module(layout, from_scope, class_path)?;
-    match layout[module_scope].classes.get(class_name.as_ref()) {
+    match find_class(layout, module_scope, class_name) {
         None => Err(CompileError::ClassDoesNotExist),
-        Some(class_index) => Ok(*class_index),
+        Some(class_index) => Ok(class_index),
     }
 }
 
@@ -77,8 +155,8 @@ pub fn resolve_func<S: AsRef<str>>(
 
     // resolve the module and func path
     let module_scope = resolve_module(layout, from_scope, func_path)?;
-    match layout[module_scope].funcs.get(func_name.as_ref()) {
+    match find_func(layout, module_scope, func_name) {
         None => Err(CompileError::ClassDoesNotExist),
-        Some(func_index) => Ok(*func_index),
+        Some(func_index) => Ok(func_index),
     }
 }
