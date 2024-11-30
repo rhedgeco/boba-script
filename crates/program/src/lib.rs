3e8159272d98e2c8a339c208f58dd948d1004a14
Ryan Hedgecock <rhedgeco@gmail.com>
2024-11-30 13:57:11 -0800

    remove program from lib

--- crates/program/src/lib.rs
+++ crates/program/src/lib.rs
@@ -1,11 +1,9 @@
 pub mod indexers;
 pub mod layout;
-pub mod program;
 pub mod resolve;
 pub mod value;
 
 pub use layout::ProgramLayout;
-pub use program::Program;
 pub use resolve::ResolvedProgram;
 
 // re-export
